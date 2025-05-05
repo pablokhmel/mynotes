fd3982ac18d322898bd52acfd9f9498ae397a163
pahmele <pavel_khmialeuski1@epam.com>
2025-05-05 20:50:14 +0200

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Pavel Khmialeuski1 on 05/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
